9a69b28e2d945db6d9ea6277f1daf4b8303cbde8
Yusuf Torun <ysftrn@gmail.com>
2026-04-22 14:59:00 +0300

    add selenium grid setup with remote webdriver

--- conftest.py
+++ conftest.py
@@ -1,13 +1,29 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+
+# LOCAL TESTING
+# @pytest.fixture
+# def driver():
+#     options = Options()
+#     options.add_argument("--headless")
+#     d = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+#     yield d
+#     d.quit()
+
+
+
+# REMOTE TESTING (VIA DOCKER)
 @pytest.fixture
 def driver():
     options = Options()
     options.add_argument("--headless")
-    d = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    d = webdriver.Remote(
+        command_executor="http://localhost:4444",
+        options=options
+    )
     yield d
     d.quit()
